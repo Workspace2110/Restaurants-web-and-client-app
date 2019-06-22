4f0b8727d901ae882b4fe64881b82e16cb77d9ae
Jheng-Hong ,Wang <workspace2110@gmail.com>
2019-06-22 11:12:00 +0800

    test
    test

---
+++ Cloud_homework4/Models/RestaurantGenreModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cloud_homework4.Models
+{
+    public class RestaurantGenreModel
+    {
+        public List<Restaurant> Restaurants;
+        public SelectList Genres;
+        public string RestaurantGenres { get; set; }
+        public string Str_search { get; set; }
+    }
+}
